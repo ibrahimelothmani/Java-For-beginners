00a74e0b8e918319ced0f6aa45a15e85b22d8553
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-11 19:45:26 -1200

    Types of Input Methods

---
+++ 4.Types of Input methods/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        Method                  Description
+//        nextBoolean()           Reads a  boolean  value from the user
+//        nextByte()              Reads a  byte  value from the user
+//        nextDouble()            Reads a  double  value from the user
+//        nextFloat()             Reads a  float  value from the user
+//        nextInt()               Reads a  int  value from the user
+//        nextLine()              Reads a  String  value from the user
+//        nextLong()              Reads a  long  value from the user
+//        nextShort()             Reads a  short  value from the user
+
+    }
+}
