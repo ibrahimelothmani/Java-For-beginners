ff0349efcf00a49105807454e455d450ecbb34ac
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-13 00:48:23 -1200

    Stack Implementation

---
+++ 34.Data Structure and algorithms/12.Stack/src/Main.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.Stack;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Stack is a data structure that follows the Last In First Out (LIFO) principle.
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        System.out.println("Stack: " + stack);
+        System.out.println("Peek: " + stack.peek());
+        System.out.println("Pop: " + stack.pop());
+        System.out.println("Stack after pop: " + stack);
+    }
+}
