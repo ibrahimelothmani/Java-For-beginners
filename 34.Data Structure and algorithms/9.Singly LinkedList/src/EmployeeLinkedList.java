6dcfae01410ce52aa890669d89603d7b55d9923b
Ibrahim <ibrahimelothmani98@gmail.com>
2025-05-13 00:25:55 -1200

    LinkedList Implementation

--- 34.Data Structure and algorithms/9.Singly LinkedList/src/EmployeeLinkedList.java
+++ 34.Data Structure and algorithms/9.Singly LinkedList/src/EmployeeLinkedList.java
@@ -3,33 +3,27 @@
 public class EmployeeLinkedList {
 
     private EmployeeNode head;
-    private EmployeeNode tail;
     private int size;
 
     public void addToFront(Employee employee) {
         EmployeeNode node = new EmployeeNode(employee);
         node.setNext(head);
         head = node;
-
-        if (tail == null) {
-            tail = head;
-        }
         size++;
     }
-
     public void addToEnd(Employee employee) {
         EmployeeNode node = new EmployeeNode(employee);
-        if (tail != null) {
-            tail.setNext(node);
-        }
-        tail = node;
-
         if (head == null) {
-            head = tail;
+            head = node;
+        } else {
+            EmployeeNode current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(node);
         }
         size++;
     }
-
     public EmployeeNode removeFromFront() {
         if (head == null) {
             return null;
@@ -37,42 +31,34 @@ public class EmployeeLinkedList {
         EmployeeNode removedNode = head;
         head = head.getNext();
         size--;
-        if (head == null) {
-            tail = null;
-        }
         return removedNode;
     }
-
     public EmployeeNode removeFromEnd() {
         if (head == null) {
             return null;
         }
-        if (head == tail) {
+        if (head.getNext() == null) {
             EmployeeNode removedNode = head;
             head = null;
-            tail = null;
             size--;
             return removedNode;
         }
         EmployeeNode current = head;
-        while (current.getNext() != tail) {
+        while (current.getNext().getNext() != null) {
             current = current.getNext();
         }
-        EmployeeNode removedNode = tail;
-        tail = current;
-        tail.setNext(null);
+        EmployeeNode removedNode = current.getNext();
+        current.setNext(null);
         size--;
         return removedNode;
     }
-
     public int getSize() {
         return size;
     }
-
     public void printList() {
         EmployeeNode current = head;
         while (current != null) {
-            System.out.println(current.getEmployee());
+            System.out.println(current);
             current = current.getNext();
         }
     }
